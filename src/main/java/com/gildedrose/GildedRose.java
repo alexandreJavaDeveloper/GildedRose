2b1ac02754eb025a85ff7bb79131d7879f77dde4
silalex <V@mo123>
2021-10-01 23:28:52 +0200

    changed from array to list the items

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,13 +1,17 @@
 package com.gildedrose;
 
+import com.gildedrose.model.Item;
+
+import java.util.List;
+
 class GildedRose {
 
     protected static final String AGED_BRIE = "Aged Brie";
     protected static final String SULFURAS_HAND_OF_RAGNAROS = "Sulfuras, Hand of Ragnaros";
     protected static final String BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT = "Backstage passes to a TAFKAL80ETC concert";
-    protected Item[] items;
+    protected List<Item> items;
 
-    public GildedRose(Item[] items) {
+    public GildedRose(List<Item> items) {
         this.items = items;
     }
 
--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,6 +1,10 @@
 package com.gildedrose;
 
+import com.gildedrose.model.Item;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -8,120 +12,138 @@ class GildedRoseTest {
 
     @Test
     void updateQuality() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        List<Item> items = new ArrayList<>();
+        items.add(new Item("foo", 0, 0));
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("foo", app.items[0].name);
-        assertEquals(0, app.items[0].quality);
-        assertEquals(-1, app.items[0].sellIn);
+        assertEquals("foo", app.items.get(0).name);
+        assertEquals(0, app.items.get(0).quality);
+        assertEquals(-1, app.items.get(0).sellIn);
     }
 
     @Test
     void updateQualityTwice() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        List<Item> items = new ArrayList<>();
+        items.add(new Item("foo", 0, 0));
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         app.updateQuality();
-        assertEquals("foo", app.items[0].name);
-        assertEquals(0, app.items[0].quality);
-        assertEquals(-2, app.items[0].sellIn);
+        assertEquals("foo", app.items.get(0).name);
+        assertEquals(0, app.items.get(0).quality);
+        assertEquals(-2, app.items.get(0).sellIn);
     }
 
     @Test
     void updateQualityWithThreeItems() {
-        Item[] items = new Item[] { new Item("foo", 0, 0),
-                new Item("foo2", 0, 0),
-                new Item("foo3", 0, 0)};
+        List<Item> items = new ArrayList<>();
+        items.add(new Item("foo", 0, 0));
+        items.add(new Item("foo2", 0, 0));
+        items.add(new Item("foo3", 0, 0));
+        
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         app.updateQuality();
-        assertEquals("foo", app.items[0].name);
-        assertEquals(0, app.items[0].quality);
-        assertEquals(-2, app.items[0].sellIn);
+        assertEquals("foo", app.items.get(0).name);
+        assertEquals(0, app.items.get(0).quality);
+        assertEquals(-2, app.items.get(0).sellIn);
 
-        assertEquals("foo2", app.items[1].name);
-        assertEquals(0, app.items[1].quality);
-        assertEquals(-2, app.items[1].sellIn);
+        assertEquals("foo2", app.items.get(1).name);
+        assertEquals(0, app.items.get(1).quality);
+        assertEquals(-2, app.items.get(1).sellIn);
 
-        assertEquals("foo3", app.items[2].name);
-        assertEquals(0, app.items[2].quality);
-        assertEquals(-2, app.items[2].sellIn);
+        assertEquals("foo3", app.items.get(2).name);
+        assertEquals(0, app.items.get(2).quality);
+        assertEquals(-2, app.items.get(2).sellIn);
     }
 
     @Test
     void updateQualityAsAgedBrie() {
-        Item[] items = new Item[] { new Item(GildedRose.AGED_BRIE, 0, 0) };
+        List<Item> items = new ArrayList<>();
+        items.add(new Item(GildedRose.AGED_BRIE, 0, 0));
+        
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         app.updateQuality();
-        assertEquals(GildedRose.AGED_BRIE, app.items[0].name);
-        assertEquals(4, app.items[0].quality);
-        assertEquals(-2, app.items[0].sellIn);
+        assertEquals(GildedRose.AGED_BRIE, app.items.get(0).name);
+        assertEquals(4, app.items.get(0).quality);
+        assertEquals(-2, app.items.get(0).sellIn);
     }
 
     @Test
     void updateQualityAsAgedBrieWithQuality() {
-        Item[] items = new Item[] { new Item(GildedRose.AGED_BRIE, 1, 10) };
+        List<Item> items = new ArrayList<>();
+        items.add(new Item(GildedRose.AGED_BRIE, 1, 10));
+        
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         app.updateQuality();
-        assertEquals(GildedRose.AGED_BRIE, app.items[0].name);
-        assertEquals(13, app.items[0].quality);
-        assertEquals(-1, app.items[0].sellIn);
+        assertEquals(GildedRose.AGED_BRIE, app.items.get(0).name);
+        assertEquals(13, app.items.get(0).quality);
+        assertEquals(-1, app.items.get(0).sellIn);
     }
 
     @Test
     void updateQualityAsSulfuras() {
-        Item[] items = new Item[] { new Item(GildedRose.SULFURAS_HAND_OF_RAGNAROS, 0, 0) };
+        List<Item> items = new ArrayList<>();
+        items.add(new Item(GildedRose.SULFURAS_HAND_OF_RAGNAROS, 0, 0));
+        
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         app.updateQuality();
-        assertEquals(GildedRose.SULFURAS_HAND_OF_RAGNAROS, app.items[0].name);
-        assertEquals(0, app.items[0].quality);
-        assertEquals(0, app.items[0].sellIn);
+        assertEquals(GildedRose.SULFURAS_HAND_OF_RAGNAROS, app.items.get(0).name);
+        assertEquals(0, app.items.get(0).quality);
+        assertEquals(0, app.items.get(0).sellIn);
     }
 
     @Test
     void updateQualityAsSulfurasWithQuality() {
-        Item[] items = new Item[] { new Item(GildedRose.SULFURAS_HAND_OF_RAGNAROS, 2, 11) };
+        List<Item> items = new ArrayList<>();
+        items.add(new Item(GildedRose.SULFURAS_HAND_OF_RAGNAROS, 2, 11));
+        
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         app.updateQuality();
-        assertEquals(GildedRose.SULFURAS_HAND_OF_RAGNAROS, app.items[0].name);
-        assertEquals(11, app.items[0].quality);
-        assertEquals(2, app.items[0].sellIn);
+        assertEquals(GildedRose.SULFURAS_HAND_OF_RAGNAROS, app.items.get(0).name);
+        assertEquals(11, app.items.get(0).quality);
+        assertEquals(2, app.items.get(0).sellIn);
     }
 
     @Test
     void updateQualityAsBackstage() {
-        Item[] items = new Item[] { new Item(GildedRose.BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT, 0, 0) };
+        List<Item> items = new ArrayList<>();
+        items.add(new Item(GildedRose.BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT, 0, 0));
+        
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         app.updateQuality();
-        assertEquals(GildedRose.BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT, app.items[0].name);
-        assertEquals(0, app.items[0].quality);
-        assertEquals(-2, app.items[0].sellIn);
+        assertEquals(GildedRose.BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT, app.items.get(0).name);
+        assertEquals(0, app.items.get(0).quality);
+        assertEquals(-2, app.items.get(0).sellIn);
     }
 
     @Test
     void updateQualityAsBackstageWithQuality() {
-        Item[] items = new Item[] { new Item(GildedRose.BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT, 3, 20) };
+        List<Item> items = new ArrayList<>();
+        items.add(new Item(GildedRose.BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT, 3, 20));
+        
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         app.updateQuality();
-        assertEquals(GildedRose.BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT, app.items[0].name);
-        assertEquals(26, app.items[0].quality);
-        assertEquals(1, app.items[0].sellIn);
+        assertEquals(GildedRose.BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT, app.items.get(0).name);
+        assertEquals(26, app.items.get(0).quality);
+        assertEquals(1, app.items.get(0).sellIn);
     }
 
     @Test
     void updateQualityAsBackstageWithQuality_11() {
-        Item[] items = new Item[] { new Item(GildedRose.BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT, 2, 11) };
+        List<Item> items = new ArrayList<>();
+        items.add(new Item(GildedRose.BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT, 2, 11));
+        
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         app.updateQuality();
-        assertEquals(GildedRose.BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT, app.items[0].name);
-        assertEquals(17, app.items[0].quality);
-        assertEquals(0, app.items[0].sellIn);
+        assertEquals(GildedRose.BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT, app.items.get(0).name);
+        assertEquals(17, app.items.get(0).quality);
+        assertEquals(0, app.items.get(0).sellIn);
     }
 }
